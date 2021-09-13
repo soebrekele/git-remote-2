d51f4481c08e44d6b14167011a0a8349388240a5
suhermanto <soehermantors@gmail.com>
2021-09-13 12:09:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eum optio saepe vel debitis quidem numquam, dolorem enim? Provident delectus eveniet nesciunt modi dignissimos repellendus aut recusandae fuga esse eligendi.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 </html>
